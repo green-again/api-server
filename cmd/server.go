068a6638d208f8fe0ff718b3e6aee73ed4377b62
임동준/SK Planet <joel.lim@buzzvil.com>
2021-10-24 18:30:45 +0900

    add testcase

--- cmd/server.go
+++ cmd/server.go
@@ -8,8 +8,10 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", HelloWorld)
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func HelloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module api-server
 
-go 1.17
+go 1.16
 
-require github.com/labstack/echo/v4 v4.6.1 // indirect
+require (
+	github.com/labstack/echo/v4 v4.6.1
+	github.com/stretchr/testify v1.4.0
+)
