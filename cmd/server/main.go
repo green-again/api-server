646620c6903794f2a610a7f4dddbba0a31839d52
DongjunLim <lacuna7304@gmail.com>
2021-11-05 23:58:58 +0900

    Integrate swaggo

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,14 +4,26 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	_ "api-server/docs"
 )
 
+// @title Green Again API server
+// @version 1.0
+// @description This is a green again backend api server
+// BasePath /v1
 func main() {
 	e := echo.New()
 	e.GET("/", HelloWorld)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
+// HelloWorld godoc
+// @Summary Print HelloWorld
+// @Success 200 {string} result "Hello, World"
+// @Router / [get]
 func HelloWorld(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module api-server
 go 1.16
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/labstack/echo/v4 v4.6.1
-	github.com/stretchr/testify v1.4.0
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/stretchr/testify v1.7.0
+	github.com/swaggo/echo-swagger v1.1.4
+	github.com/swaggo/swag v1.7.4 // indirect
+	golang.org/x/net v0.0.0-20211104170005-ce137452f963 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/tools v0.1.7 // indirect
 )
