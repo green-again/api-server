90c96583b56103cb226a7176d4ea2576effe9bda
woojin <66457043+c-woojin@users.noreply.github.com>
2021-11-30 20:40:24 +0900

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,5 +25,5 @@ func main() {
 // @Success 200 {string} result "Hello, World"
 // @Router / [get]
 func HelloWorld(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, Green-Again!")
+	return c.String(http.StatusOK, "Hello, Green-Again! Let's Start!")
 }
